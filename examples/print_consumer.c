be05a95dc41ef306892a80c12187c266b0c3b4ce
Xinyu Ma <bitmxy@gmail.com>
2019-03-10 09:40:00 -0700

    Try to fix the SD. Best effort.

--- examples/print_consumer.c
+++ examples/print_consumer.c
@@ -6,16 +6,27 @@
 //  Copyright © 2019 shanxigy. All rights reserved.
 //
 
-
+#include <stdio.h>
+#include <unistd.h>
 #include "ndn-lite/security/ndn-lite-crypto-key.h"
 #include "ndn-lite/app-support/service-discovery.h"
 #include "ndn-lite/encode/signed-interest.h"
 #include "ndn-lite/security/ndn-lite-hmac.h"
 #include "ndn-lite/security/ndn-lite-ecc.h"
 #include "ndn-lite/ndn-services.h"
+#include "ndn-lite/encode/key-storage.h"
+#include "adaptation/udp-unicast/ndn-udp-unicast-face.h"
+#include "ndn-lite/face/direct-face.h"
 
 ndn_ecc_pub_t* pub_key = NULL;
 ndn_ecc_prv_t* prv_key = NULL;
+
+const uint8_t prv[] = {
+    0x5D, 0xC7, 0x6B, 0xAB, 0xEE, 0xD4, 0xEB, 0xB7, 0xBA, 0xFC,
+    0x64, 0xE7, 0x8B, 0xDB, 0x22, 0xE1, 0xF4, 0x37, 0x10, 0xC2,
+    0xEA, 0xE9, 0xDD, 0xAF, 0xF4, 0x74, 0xB3, 0x18, 0x08, 0x56,
+    0x5E, 0x4C
+};
 
 const uint8_t pub[] = {
     0x36, 0xF7, 0xEF, 0x7C, 0x05, 0x10, 0x68, 0xC4, 0x6C, 0x67,
@@ -27,34 +38,39 @@ const uint8_t pub[] = {
     0xE8, 0xBA, 0x21, 0x89
 };
 
+ndn_udp_unicast_face_t *face;
+
 int on_advertisement(const uint8_t* rawdata, uint32_t data_size){
-    ndn_interest_t* decoded_interest;
-    int ret_val = ndn_interest_from_block(decoded_interest, rawdata, data_size);
+    ndn_interest_t decoded_interest;
+    int ret_val = ndn_interest_from_block(&decoded_interest, rawdata, data_size);
     if (ret_val != 0) {
-        print_error("Consumer", "on_advertisement", "ndn_data_tlv_decode_ecdsa_verify", ret_val);
+        printf("ERROR: ndn_interest_from_block (%d)\n", ret_val);
+        return ret_val;
     }
     
-    ndn_sd_on_advertisement_process(decoded_interest);
+    ndn_sd_on_advertisement_process(&decoded_interest);
     
     return NDN_SUCCESS;
 }
 
 int on_query_response(const uint8_t* rawdata, uint32_t data_size){
-    ndn_interest_t* decoded_interest;
+    ndn_data_t data;
     
-    int ret_val = ndn_data_tlv_decode_ecdsa_verify(decoded_interest, data, data_size, pub_key);
+    int ret_val = ndn_data_tlv_decode_ecdsa_verify(&data, rawdata, data_size, pub_key);
     if (ret_val != 0) {
-        print_error("provider", "on_query_response", "ndn_data_tlv_decode_ecdsa_verify", ret_val);
+        printf("ERROR: ndn_data_tlv_decode_ecdsa_verify (%d)\n", ret_val);
+        return ret_val;
     }
     
-    ndn_sd_on_query_response_process(decoded_interest);
+    ndn_sd_on_query_response_process(&data);
     
     return NDN_SUCCESS;
 }
 
-
 int main()
 {
+    int ret_val;
+
     // tests start
     ndn_security_init();
     
@@ -66,48 +82,49 @@ int main()
     ret_val = ndn_ecc_prv_init(prv_key, prv, sizeof(prv),
                                NDN_ECDSA_CURVE_SECP256R1, 123);
     if (ret_val != 0) {
-        print_error("_Consumer", "init keys", "ndn_ecc_prv_init", ret_val);
-        return -1;
+        printf("ERROR: ndn_ecc_prv_init (%d)\n", ret_val);
+        return ret_val;
     }
     ret_val = ndn_ecc_pub_init(pub_key, pub, sizeof(pub),
                                NDN_ECDSA_CURVE_SECP256R1, 456);
     if (ret_val != 0) {
-        print_error("_Consumer", "init keys", "ndn_ecc_pub_init", ret_val);
-        return -1;
+        printf("ERROR: ndn_ecc_pub_init (%d)\n", ret_val);
+        return ret_val;
     }
     
     // set home prefix
     ndn_name_t home_prefix;
     char* home_prefix_str = "/ndn";
-    int ret_val = ndn_name_from_string(&home_prefix, home_prefix_str, sizeof(home_prefix_str));
+    ret_val = ndn_name_from_string(&home_prefix, home_prefix_str, strlen(home_prefix_str));
     if (ret_val != 0) {
-        print_error(_current_test_name, "_Consumer", "ndn_name_from_string", ret_val);
-        _all_function_calls_succeeded = false;
+        printf("ERROR: ndn_name_from_string (%d)\n", ret_val);
+        return ret_val;
     }
-    
     
     // set consumer components
     char comp_consumer[] = "/ndn/consumer";
-    name_component_t component_consumer;
-    ret_val = name_component_from_string(&component_consumer, comp_consumer, sizeof(comp_consumer));
+    ndn_name_t component_consumer;
+    ret_val = ndn_name_from_string(&component_consumer, comp_consumer, strlen(comp_consumer));
     if (ret_val != 0) {
-        print_error(_current_test_name, "_Consumer", "name_component_from_string", ret_val);
-        _all_function_calls_succeeded = false;
+        printf("ERROR: name_component_from_string (%d)\n", ret_val);
+        return ret_val;
     }
     
-    
+    face = ndn_udp_unicast_face_construct(1, INADDR_ANY, 6000, htonl(INADDR_LOOPBACK), 5000);
+    ndn_direct_face_construct(2);
+
     // intialization
-    ret_val = ndn_direct_face_register_prefix(comp_consumer, on_advertisement);
+    ret_val = ndn_direct_face_register_prefix(&component_consumer, on_advertisement);
   
 
     //adding FIB entry
     ndn_name_t name;
     ndn_name_from_string(&name, "/ndn", 4);
-    ndn_forwarder_fib_insert(name, &face->intf, 0);
+    ndn_forwarder_fib_insert(&name, &face->intf, 0);
     
     //find the print service
     char service_need[] = "/ndn/SD/Yu/print";
-    int service_size = sizeof(service_need);
+    //int service_size = sizeof(service_need);
     ndn_sd_identity_t* entry;
     
     ndn_interest_t interest;
@@ -141,19 +158,16 @@ int main()
     
     
     //send a query every second, until service state is AVAILABLE
-    ndn_udp_unicast_face_t *face;
-    face = ndn_udp_multicast_face_construct(1, INADDR_ANY, 12345, inet_aton("225.0.0.37"));
-    ndn_direct_face_construct(2);
     
     ndn_interest_t query;
     while (entry->services[service_number].status != NDN_APPSUPPORT_SERVICE_AVAILABLE) {
         puts("Consumer: service state not available");
         ndn_sd_prepare_query(&query, &entry->identity, &entry->services[service_number],
                          NULL, 0);
-        encoder_init(&encoder, buf, 4096);
-        ret_val = ndn_signed_interest_ecdsa_sign(&query, &consumer_identity, &prv_key);
+        encoder_init(&encoder, buffer, 1024);
+        ret_val = ndn_signed_interest_ecdsa_sign(&query, &consumer_identity, prv_key);
         ret_val = ndn_interest_tlv_encode(&encoder, &query);
-        ndn_direct_face_express_interest(&name_prefix, encoder.output_value, encoder.offset, on_query_response, NULL);
+        ndn_direct_face_express_interest(&query.name, encoder.output_value, encoder.offset, on_query_response, NULL);
         puts("Query");
         usleep(1000);
     }
@@ -162,7 +176,6 @@ int main()
     
     //send an evoke Interest
     ndn_name_t name_prefix;
-    ndn_interest_t interest;
     uint8_t buf[4096];
     char name_string[] = "/ndn/SD/Yu/print/(3+5)*2-6";
     if(ndn_name_from_string(&name_prefix, name_string, strlen(name_string)) != NDN_SUCCESS){
@@ -171,15 +184,15 @@ int main()
     }
     ndn_interest_from_name(&interest, &name_prefix);
     encoder_init(&encoder, buf, 4096);
-    ret_val = ndn_signed_interest_ecdsa_sign(&interest, &consumer_identity, &prv_key);
+    ret_val = ndn_signed_interest_ecdsa_sign(&interest, &consumer_identity, prv_key);
     if (ret_val != 0) {
-        print_error(_current_test_name, "_Consumer", "ndn_signed_interest_ecdsa_sign", ret_val);
-        _all_function_calls_succeeded = false;
+        printf("ERROR: ndn_signed_interest_ecdsa_sign (%d)\n", ret_val);
+        return ret_val;
     }
     ret_val = ndn_interest_tlv_encode(&encoder, &interest);
     if (ret_val != 0) {
-        print_error(_current_test_name, "_Consumer", "ndn_interest_tlv_encode", ret_val);
-        _all_function_calls_succeeded = false;
+        printf("ERROR: ndn_interest_tlv_encode (%d)\n", ret_val);
+        return ret_val;
     }
     
     printf("Value of interest, encoded:\n");
@@ -192,7 +205,7 @@ int main()
     ndn_direct_face_express_interest(&name_prefix, encoder.output_value, encoder.offset, NULL, NULL);
 
     while(1){
-        ndn_udp_multicast_face_recv(face);
+        ndn_udp_unicast_face_recv(face);
         usleep(10);
     }
     
--- examples/print_producer.c
+++ examples/print_producer.c
@@ -7,13 +7,19 @@
 //
 
 #include <unistd.h>
-
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "ndn-lite/security/ndn-lite-crypto-key.h"
 #include "ndn-lite/app-support/service-discovery.h"
 #include "ndn-lite/encode/signed-interest.h"
 #include "ndn-lite/security/ndn-lite-hmac.h"
 #include "ndn-lite/security/ndn-lite-ecc.h"
 #include "ndn-lite/ndn-services.h"
+#include "ndn-lite/encode/key-storage.h"
+#include "adaptation/udp-unicast/ndn-udp-unicast-face.h"
+#include "ndn-lite/face/direct-face.h"
 
 const uint8_t prv[] = {
     0x5D, 0xC7, 0x6B, 0xAB, 0xEE, 0xD4, 0xEB, 0xB7, 0xBA, 0xFC,
@@ -32,20 +38,52 @@ const uint8_t pub[] = {
     0xE8, 0xBA, 0x21, 0x89
 };
 
+ndn_udp_unicast_face_t *face;
+uint8_t buf[4096];
 
 int on_interest(const uint8_t* interest, uint32_t interest_size){
-    ndn_interest_t* decoded_interest;
-    ret_val = ndn_interest_from_block(decoded_interest, interest, interest_size);
+    ndn_interest_t decoded_interest;
+    int ret_val;
+    ret_val = ndn_interest_from_block(&decoded_interest, interest, interest_size);
     if (ret_val != 0) {
-        print_error(_current_test_name, "_run_service_discovery_test", "ndn_interest_from_block", ret_val);
+        printf("ERROR: Unable to decode Interest. (%d)\n", ret_val);
+        return ret_val;
     }
-    name_component_t command = decoded_interest->name.components[3];
-    puts(command.value);
+    name_component_t command = decoded_interest.name.components[3];
+    printf("OnInterest: %s\n", command.value);
+    return NDN_SUCCESS;
+}
+
+int on_query_interest(const uint8_t* interest, uint32_t interest_size){
+    ndn_interest_t decoded_interest;
+    ndn_data_t response;
+    ndn_encoder_t encoder;
+    int ret_val;
+    ret_val = ndn_interest_from_block(&decoded_interest, interest, interest_size);
+    if (ret_val != 0) {
+        printf("ERROR: Unable to decode Interest. (%d)\n", ret_val);
+        return ret_val;
+    }
+    printf("OnQueryInterest\n");
+    ret_val = ndn_sd_on_query_process(&decoded_interest, &response);
+    if (ret_val != 0) {
+        printf("ERROR: ndn_sd_on_query_process. (%d)\n", ret_val);
+        return ret_val;
+    }
+    encoder_init(&encoder, buf, 4096);
+    ndn_data_tlv_encode_digest_sign(&encoder, &response);
+    ndn_forwarder_on_incoming_data(ndn_forwarder_get_instance(),
+                                   &face->intf,
+                                   &response.name,
+                                   encoder.output_value,
+                                   encoder.offset);
     return NDN_SUCCESS;
 }
 
 int main()
 {
+    int ret_val;
+
     // tests start
     ndn_security_init();
     
@@ -55,20 +93,12 @@ int main()
     // set home prefix
     ndn_name_t home_prefix;
     char* home_prefix_str = "/ndn";
-    ret_val = ndn_name_from_string(&home_prefix, home_prefix_str, sizeof(home_prefix_str));
-    if (ret_val != 0) {
-        print_error(_current_test_name, "_run_service_discovery_test", "ndn_name_from_string", ret_val);
-        _all_function_calls_succeeded = false;
-    }
+    ndn_name_from_string(&home_prefix, home_prefix_str, sizeof(home_prefix_str));
     
     // set producer components
     char comp_producer[] = "/ndn/producer";
     name_component_t component_producer;
-    ret_val = name_component_from_string(&component_producer, comp_producer, sizeof(comp_producer));
-    if (ret_val != 0) {
-        print_error(_current_test_name, "_run_service_discovery_test", "ndn_name_component_from_string", ret_val);
-        _all_function_calls_succeeded = false;
-    }
+    name_component_from_string(&component_producer, comp_producer, sizeof(comp_producer));
     
     
     ndn_interest_t interest;
@@ -93,13 +123,13 @@ int main()
     ret_val = ndn_ecc_prv_init(prv_key, prv, sizeof(prv),
                                NDN_ECDSA_CURVE_SECP256R1, 123);
     if (ret_val != 0) {
-        print_error("controller", "init keys", "ndn_ecc_prv_init", ret_val);
+        printf("ndn_ecc_prv_init error (%d)\n", ret_val);
         return -1;
     }
     ret_val = ndn_ecc_pub_init(pub_key, pub, sizeof(pub),
                                NDN_ECDSA_CURVE_SECP256R1, 456);
     if (ret_val != 0) {
-        print_error("controller", "init keys", "ndn_ecc_pub_init", ret_val);
+        printf("ndn_ecc_pub_init error (%d)\n", ret_val);
         return -1;
     }
 
@@ -110,35 +140,36 @@ int main()
     printf("Advertisement Preparation Success\n");
     ret_val = ndn_interest_tlv_encode(&encoder, &interest);
     if (ret_val != 0) {
-        print_error(_current_test_name, "_run_service_discovery_test",  "ndn_interest_tlv_encode", ret_val);
-        _all_function_calls_succeeded = false;
+        printf("ndn_interest_tlv_encode error (%d)\n", ret_val);
+        return -1;
     }
     
-    face = ndn_udp_multicast_face_construct(1, INADDR_ANY, 12345, inet_aton("225.0.0.37"));
+    // TODO: Use unicast for debugging
+    face = ndn_udp_unicast_face_construct(1, INADDR_ANY, 5000, htonl(INADDR_LOOPBACK), 6000);
     ndn_direct_face_construct(2);
     
     //adding FIB entry
     ndn_name_t name;
     ndn_name_from_string(&name, "/ndn", 4);
-    ndn_forwarder_fib_insert(name, &face->intf, 0);
-    
+    ndn_forwarder_fib_insert(&name, &face->intf, 0);
     
     //register prefix
     char query_prefix[] = "/ndn/SD/Yu/QUERY";
-    ndn_direct_face_register_prefix(&query_prefix, ndn_sd_on_query_process);
+    ndn_name_from_string(&name, query_prefix, strlen(query_prefix));
+    ndn_direct_face_register_prefix(&name, on_query_interest);
     char evoke_prefix[] = "/ndn/SD/Yu/print";
-    ndn_direct_face_register_prefix(&evoke_prefix, on_interest);
+    ndn_name_from_string(&name, evoke_prefix, strlen(evoke_prefix));
+    ndn_direct_face_register_prefix(&name, on_interest);
     
     
     //periodically sending advertisement interest
     while (1) {
-        ndn_direct_face_express_interest(&name_prefix, encoder.output_value, encoder.offset, NULL, NULL);
+        ndn_direct_face_express_interest(&interest.name, encoder.output_value, encoder.offset, NULL, NULL);
         for (int i = 0; i < 1000; ++i) {
-            ndn_udp_multicast_face_recv(face);
+            ndn_udp_unicast_face_recv(face);
             usleep(10);
         }
     }
     
     ndn_face_destroy(&face->intf);
-    
 }
